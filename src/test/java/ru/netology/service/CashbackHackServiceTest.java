47b02a8e04b73f4f7d75da02143c486b60581ae8
Марина Безотеческая <bezotecheskaya@mail.ru>
2022-07-17 12:18:42 +0700

    "testNG"

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,13 +1,13 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.*;
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void test1() {
+    public void testRemain() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 800;
         int actual = cashbackHackService.remain(amount);
@@ -16,12 +16,11 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void test2() {
+    public void testRemain1() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
         assertEquals(actual, expected);
     }
-
 }
